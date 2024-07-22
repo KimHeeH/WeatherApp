7a32cfac9bbdc3ba8db5b5c3d4d87280f7a61987
KimHeeSoo <khstoday@naver.com>
2024-07-22 14:57:16 +0900

    최종수정

--- OneDrive/바탕 화면/weather-app/weather/src/App.js
+++ OneDrive/바탕 화면/weather-app/weather/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 //1.앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
 //2.날씨정보에는 도시,섭씨,화씨의 날씨상태
 //3.5개의 버튼이 있다.(1개는 현재위치 4개는 다른 도시)
@@ -11,6 +12,10 @@ import WeatherButton from "./component/WeatherButton";
 //6.데이터를 들고오는 동안 로딩 스피너가 돈다.
 function App() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState("false");
+  const [color, setColor] = useState("green");
+  const cities = ["paris", "new york", "tokyo", "seoul"];
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -22,25 +27,68 @@ function App() {
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=43134dbd7d6b477b80e801bca836e039&units=metric`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    setWeather(data);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=43134dbd7d6b477b80e801bca836e039&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+      setWeather(data);
+      setCity("");
+    } catch (err) {
+      console.log(err);
+      alert("Failed to fetch weather data for the current location.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getWeatherByCity = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=43134dbd7d6b477b80e801bca836e039&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+      setWeather(data);
+      setColor("#ffffff");
+    } catch (err) {
+      console.log(err);
+      alert(`Failed to fetch weather data for ${city}.`);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    getCurrentLocation();
-  }, []);
+    if (city == "") {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]);
 
   return (
     <div>
-      <div className="container">
-        <div className="SphereContainer">
-          <WeatherBox weather={weather} />
-          <WeatherButton />
+      {loading ? (
+        <div className="container">
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
         </div>
-      </div>
+      ) : (
+        <div className="container">
+          <div className="SphereContainer">
+            <WeatherBox weather={weather} />
+
+            <WeatherButton
+              cities={cities}
+              setCity={setCity}
+              color={color}
+              selectedCity={city}
+              getCurrentLocation={getCurrentLocation}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- OneDrive/바탕 화면/weather-app/weather/src/component/WeatherButton.js
+++ OneDrive/바탕 화면/weather-app/weather/src/component/WeatherButton.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = () => {
+const WeatherButton = ({
+  cities,
+  setCity,
+  color,
+  selectedCity,
+  getCurrentLocation,
+}) => {
+  console.log(cities);
+
   return (
     <div>
       <Button
@@ -9,31 +17,28 @@ const WeatherButton = () => {
           border: "1px solid rgb(158, 158, 158)",
           color: "black",
           margin: 10,
+          backgroundColor: selectedCity === "" ? "white" : "transparent",
         }}
         variant="outline-light"
+        onClick={getCurrentLocation}
       >
         Current Location
       </Button>
-      <Button
-        style={{
-          border: "1px solid rgb(158, 158, 158)",
-          color: "black",
-          margin: 10,
-        }}
-        variant="outline-light"
-      >
-        Paris
-      </Button>
-      <Button
-        style={{
-          border: "1px solid rgb(158, 158, 158)",
-          color: "black",
-          margin: 10,
-        }}
-        variant="outline-light"
-      >
-        New York
-      </Button>
+      {cities.map((item, index) => (
+        <Button
+          style={{
+            border: "1px solid rgb(158, 158, 158)",
+            color: "black",
+            margin: 10,
+            backgroundColor: selectedCity === item ? "white" : "transparent",
+          }}
+          variant="outline-light"
+          key={index}
+          onClick={() => setCity(item)}
+        >
+          {item}
+        </Button>
+      ))}
     </div>
   );
 };
